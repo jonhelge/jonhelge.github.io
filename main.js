0b643e35ec8348f5c8c4c61cc23ac8c4ab85163d
Jon Helge Nistad <jon.helge.nistad@nordicsemi.no>
2015-11-26 11:10:28 +0100

    testing

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-var toastGroupTemplate = document.querySelector('#toastGroup');
-toastGroupTemplate.showToast = function() {
-  document.querySelector('#toast').show();
-}
+document.addEventListener('WebComponentsReady', function() {
+
+  var batteryDevice = document.querySelector('platinum-bluetooth-device');
+  var button = document.querySelector('paper-button');
+
+  button.addEventListener('click', function() {
+    console.log('Requesting a bluetooth device advertising a battery service...');
+    
+    batteryDevice.request().then(function(device) {
+      console.log('A bluetooth device has been found!');
+      console.log('Device Name: ' + device.name);
+    })
+    .catch(function(error) {
+      console.error('Argh! ', error);
+    });
+  });
+});
